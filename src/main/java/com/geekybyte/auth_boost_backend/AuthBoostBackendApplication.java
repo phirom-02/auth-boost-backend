177c2b912293e16136d6aef3f2783308df270a25
phirom-02 <phiromkhim02@gmail.com>
2025-07-11 20:47:50 +0700

    Initial commit

---
+++ src/main/java/com/geekybyte/auth_boost_backend/AuthBoostBackendApplication.java
@@ -0,0 +1,13 @@
+package com.geekybyte.auth_boost_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthBoostBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthBoostBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekybyte/auth_boost_backend/AuthBoostBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekybyte.auth_boost_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthBoostBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
